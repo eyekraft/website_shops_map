662cbdee4b12ea7771b84b68c44976863d37222a
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2022-01-30 15:58:43 -0400

    [IMP] Added more improvements about QA in the Python source code

--- models/shop_api_user.py
+++ models/shop_api_user.py
@@ -6,14 +6,14 @@ from odoo import api, fields, models
 class shop_api_user(models.Model):
     _name = 'shop.api.user'
     _description = 'Shop API users'
-    
+
     def generate_api_key(self):
         """
         Generate API key for shop list user
         """
         hash_dict = {field: self.__getattribute__(field) for field in self._fields}
         self.api_key = hashlib.sha1(str(hash_dict).encode('utf-8')).hexdigest()
-    
+
     @api.model
     def create(self, values):
         api_user = super(shop_api_user, self).create(values)
