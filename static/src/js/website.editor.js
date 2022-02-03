5734227a6a709585be340aa297452ad5eafdbfe6
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2022-02-03 15:38:17 -0400

    [ADD] Added more improvements about QA in the Javascript source code

--- static/src/js/website.editor.js
+++ static/src/js/website.editor.js
@@ -1,36 +1,36 @@
 odoo.define('website_shops_map.editor', function (require) {
-'use strict';
+    'use strict';
 
-var core = require('web.core');
-var editor = require('web_editor.editor');
-var website = require('website.website');
-var _t = core._t;
+    var core = require('web.core');
+    var editor = require('web_editor.editor');
+    var website = require('website.website');
+    var _t = core._t;
 
-website.TopBar.include({
-    /**
-     * Edit function.
-     *
-     */
-    edit: function () {
-        this.$('button[data-action=edit]').prop('disabled', true);
-        this.$el.hide();
-        editor.editor_bar = new editor.Class(this);
-        editor.editor_bar.prependTo(document.body);
-        message_list = _t('Select property');
+    website.TopBar.include({
+        /**
+         * Edit function.
+         *
+         */
+        edit: function () {
+            this.$('button[data-action=edit]').prop('disabled', true);
+            this.$el.hide();
+            editor.editor_bar = new editor.Class(this);
+            editor.editor_bar.prependTo(document.body);
+            message_list = _t('Select property');
 
-        var selector = $(document).find('#eyekraft_props_picker_selector')
-        if (selector.length){
-            // remove properties
-            $(selector).children().remove();
-            $(selector).append("<select id='eyekraft_props_picker' class='hidden' multiple='' title='+ message_list +'></select>");
-        };
+            var selector = $(document).find('#eyekraft_props_picker_selector')
+            if (selector.length){
+                // remove properties
+                $(selector).children().remove();
+                $(selector).append("<select id='eyekraft_props_picker' class='hidden' multiple='' title='+ message_list +'></select>");
+            };
 
-        var big_map_view = $(document).find('#map-container-vertical')
-        if (big_map_view.length){
-            // remove big map
-            $(big_map_view).children().remove();
-        };
-    }
-});
+            var big_map_view = $(document).find('#map-container-vertical')
+            if (big_map_view.length){
+                // remove big map
+                $(big_map_view).children().remove();
+            };
+        }
+    });
 
 });
