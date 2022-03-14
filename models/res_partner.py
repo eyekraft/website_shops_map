f2a28e0d32e9dc6a2ac752762b4c7469f5d3dc10
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2022-03-14 05:47:34 -0400

    [FIX] Fixed Too many blank lines

--- models/res_partner.py
+++ models/res_partner.py
@@ -7,7 +7,6 @@ _logger = logging.getLogger(__name__)
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-
     # Model Fields
     partner_latitude = fields.Float(string='Geo Latitude', digits=(16, 5))
     partner_longitude = fields.Float(string='Geo Longitude', digits=(16, 5))
